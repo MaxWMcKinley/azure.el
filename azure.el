e7475c867e98e816ba3dd58c5e9355c9d382179d
stevenfontanella <--get>
2020-07-27 15:30:53 -0700

    refactor

--- azure.el
+++ azure.el
@@ -20,7 +20,7 @@
     (json-read-from-string str)))
 
 (aio-defun request-with-header (token url)
-  "docstring"
+  "Make a request to `url` with the given `token` as the bearer token"
   (let ((url-request-method "GET")
 	 (url-request-extra-headers
 	  `(("Content-Type" . "application/json")
@@ -29,6 +29,7 @@
 
 ;; curl -X POST -d 'grant_type=client_credentials&client_id=[APP_ID]&client_secret=[PASSWORD]&resource=https%3A%2F%2Fmanagement.azure.com%2F' https://login.microsoftonline.com/[TENANT_ID]/oauth2/token
 (aio-defun get-oauth (app-id tenant password)
+  "Return the bearer token"
   (let ((url-request-method "POST")
 	(url-request-data (format "grant_type=client_credentials&client_id=%s&client_secret=%s&resource=https%%3A%%2F%%2Fmanagement.azure.com%%2F" app-id password))
 	(resp (aio-await (aio-url-retrieve (format "https://login.microsoftonline.com/%s/oauth2/token" tenant))))
@@ -36,14 +37,23 @@
     (with-current-buffer buf
       (buffer-string))))
 
-
-(aio-defun azure--login ()
-  "return bearer token for future requests"
+(aio-defun create-principal ()
+  "Create service principal"
   (let*
     ((json (json-parse (shell-command-to-string "az ad sp create-for-rbac --name azure.el 2>/dev/null")))
      (password (gethash "password" json))
      (tenant (gethash "tenant" json))
      (app-id (gethash "appId" json)))
-    (aio-await (get-oauth app-id tenant password))))
+   `((app-id . ,app-id) (tenant . ,tenant) (password . ,password))))
 
-(setq token (aio-wait-for (azure--login)))
+(aio-defun azure--login ()
+  "Populate the global bearer-token for use with future requests"
+  (let*
+    ((alist (aio-await (create-principal)))
+    (app-id (alist-get 'app-id alist))
+    (tenant (alist-get 'tenant alist))
+    (password (alist-get 'password alist))
+    (token (aio-await (get-oauth app-id tenant password))))
+   (setq bearer-token token)))
+
+(aio-wait-for (azure--login))
