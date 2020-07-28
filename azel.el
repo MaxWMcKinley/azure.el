0c0163b45340afd0f8afc5072ea0a14043c58860
Max Mckinley <max@MacBook-Pro.local>
2020-07-28 13:56:50 -0500

    Created azure major mode

--- azel.el
+++ azel.el
@@ -3,12 +3,17 @@
         '("azel-test-ase" "Down" "azel-test" "test-free")
         '("azel-test-db" "Running" "azel-test" "test-free")))
 
-(get-resources)
+(define-derived-mode azure-mode tabulated-list-mode "Azure"
+  "Azure Mode"
+  (let ((columns [("Resource" 25) ("Status" 25) ("Group" 25) ("Subscription" 25)])
+        (rows (mapcar (lambda (x) `(nil ,(vconcat x))) (get-resources))))
+    (switch-to-buffer "*temp*")
+    (setq tabulated-list-format columns)
+    (setq tabulated-list-entries rows)
+    (tabulated-list-init-header)
+    (tabulated-list-print)))
 
-(let ((columns [("Resource" 25) ("Status" 25) ("Group" 25) ("Subscription" 25)])
-      (rows (mapcar (lambda (x) `(nil ,(vconcat x))) (get-resources))))
-  (switch-to-buffer "*temp*")
-  (setq tabulated-list-format columns)
-  (setq tabulated-list-entries rows)
-  (tabulated-list-init-header)
-  (tabulated-list-print))
+(defun azure ()
+  (interactive)
+  (switch-to-buffer "*azure*")
+  (azure-mode))
