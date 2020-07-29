be1983d1bf72153139f75abb1bbb85bae3393006
Max Mckinley <max@MacBook-Pro.local>
2020-07-28 21:34:30 -0500

    Fetch resource list using az

--- azel.el
+++ azel.el
@@ -1,17 +1,34 @@
-(defun get-resources ()
-  (list '("azel-test-func" "Running" "azel-test" "test-free")
-        '("azel-test-ase" "Down" "azel-test" "test-free")
-        '("azel-test-db" "Running" "azel-test" "test-free")))
+;; Parse json as a hashtable, basically a repeat of what is in azure.el
+(defun json-parse (str)
+  "Parse json from str as hashtable"
+  (let ((json-object-type 'hash-table)
+        (json-array-type 'list))
+    (json-read-from-string str)))
 
+;; Remove any resources of "null" kind
+(defun filter-resources (res)
+  (seq-filter (lambda (x) (gethash "kind" x)) res))
+
+;; Fetch resource list with az, then format result as a vector in the format tabulated-list-mode can read
+(defun get-resources ()
+  (let ((res (filter-resources (json-parse (shell-command-to-string "az resource list")))))
+    (mapcar (lambda (x) (vector (gethash "name" x)
+                                (gethash "kind" x)
+                                (gethash "location" x)
+                                (gethash "resourceGroup" x))
+              ) res)))
+
+;; Create azure major made based on tabulated list
 (define-derived-mode azure-mode tabulated-list-mode "Azure"
   "Azure Mode"
-  (let ((columns [("Resource" 25) ("Status" 25) ("Group" 25) ("Subscription" 25)])
-        (rows (mapcar (lambda (x) `(nil ,(vconcat x))) (get-resources))))
+  (let ((columns [("Resource" 20) ("Kind" 20) ("Location" 20) ("Resource Group" 20)]); ("Tags" 20)])
+        (rows (mapcar (lambda (x) `(nil ,x)) (get-resources))))
     (setq tabulated-list-format columns)
     (setq tabulated-list-entries rows)
     (tabulated-list-init-header)
     (tabulated-list-print)))
 
+;; Interactive function to launch azel
 (defun azure ()
   (interactive)
   (switch-to-buffer "*azure*")
